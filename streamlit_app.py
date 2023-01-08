66891f67a8aa23886b6bfc86cf6c56691aee919d
ancbddsfre <122067322+ancbddsfre@users.noreply.github.com>
2023-01-08 23:45:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Kiwi") #+ fruit_choice)
 streamlit.write('Displaying information for: ', fruit_choice)
 
 # normalize json responnse
