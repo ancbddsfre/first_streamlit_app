a83c06ad8fdd684ec55ff35e94796179a66356b9
ancbddsfre <122067322+ancbddsfre@users.noreply.github.com>
2023-01-08 23:43:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,13 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.write('Displaying information for: ', fruit_choice)
+
+# normalize json responnse
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it as a table
+streamlit.dataframe(fruityvice_normalized)
 
