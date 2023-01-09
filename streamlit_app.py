88f641006484b4214f126ed66b702f8cfec770a2
ancbddsfre <122067322+ancbddsfre@users.noreply.github.com>
2023-01-09 01:47:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,3 @@ if streamlit.button('Add a fruit to the list'):
 
 #my_data_rows.append( (add_fruit_prompt,) )
 #streamlit.dataframe(my_data_rows)
-
-
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
