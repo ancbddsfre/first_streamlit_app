4fd0bccc6e293cc4598d6453160e666c7ef7002f
ancbddsfre <122067322+ancbddsfre@users.noreply.github.com>
2023-01-09 01:24:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_fruit_prompt = streamlit.text_input('What fruit would you like to add?')
-streamlit.text("Thanks for adding ", add_fruit_prompt)
+streamlit.text("Thanks for adding " + add_fruit_prompt)
 #my_data_rows.append( (add_fruit_prompt,) )
 #streamlit.dataframe(my_data_rows)
 
