9014e610a06154dae9e22a4a9e4908a143b06f02
ancbddsfre <122067322+ancbddsfre@users.noreply.github.com>
2023-01-09 01:39:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,11 @@ def get_fruityvice_data(fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
   
 
 
@@ -43,18 +48,25 @@ except URLError as e:
   streamlit.error()
 
 
-streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
+streamlit.header("The fruit load list contains:")
+
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+  streamlit.stop()
+  
 add_fruit_prompt = streamlit.text_input('What fruit would you like to add?')
 streamlit.text("Thanks for adding " + add_fruit_prompt)
 #my_data_rows.append( (add_fruit_prompt,) )
 #streamlit.dataframe(my_data_rows)
+
+
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
